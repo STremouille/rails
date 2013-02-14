221af7a9c524abdde456162c1525863376e7a1d4
STremouille <samuel.tremouille@gmail.com>
2013-02-14 14:14:21 +0100

    refactoring script installation

--- ror_app/install.sh
+++ ror_app/install.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
+
+install()
+{
+#get sudo privilege
+sudo su
+#cd to user dir
+cd ~;
+
 #rvm :
-sudo apt-get install -y build-essential curl zlib1g-dev libreadline-dev libssl-dev libxml2-dev
+sudo apt-get install -y build-essential curl zlib1g-dev libreadline-dev libssl-dev libxml2-dev rails3 sqlite3 libsqlite3-dev git;
 
 ##Separation ?
-curl https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable
+curl https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable;
 #Complete path for rvm because new bashrc isn't load yet
-.rvm/bin/rvm reload
+.rvm/bin/rvm reload;
 
 #ruby :
-.rvm/bin/rvm install 1.9.2
-
-#rails : 
-sudo apt-get install -y rails3
-
-#git:
-sudo apt-get install -y git
+.rvm/bin/rvm install 1.9.2;
 
 #rapatriement du projet:
-git clone https://github.com/STremouille/rails.git
-mv rails/ projet-rails/
+git clone https://github.com/STremouille/rails.git;
+mv rails/ projet-rails/;
 
 #intall all gems:
-sudo apt-get install -y sqlite3 libsqlite3-dev
-sudo gem install sqlite3 -v '1.3.7'
-cd projet-rails/ror_app/
-bundle install
+
+sudo gem install sqlite3 -v '1.3.7';
+cd projet-rails/ror_app/;
+bundle install;
 
 #migrate de la bdd
-rake db:migrate
+rake db:migrate;
+}
+
+install
